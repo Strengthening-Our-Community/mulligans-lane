ac914d0bd489c937e1f78aa3e10982f2b68106f6
Alan Keown <BurrellCreekCommunity@gmail.com>
2021-07-24 16:11:48 +1000

    Deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,19 @@
+import adapter from '@sveltejs/adapter-static';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		adapter: adapter({
+			// default options are shown
+			pages: 'build/_app/pages',
+			assets: 'build/_app/assets',
+			fallback: null
+		}),
+		// Comment the paths if wants to run in dev mode.
+		paths: {
+			base: '/Strengthening-Our-Community/mulligans-lane',
+			assets: '/Strengthening-Our-Community/mulligans-lane'
+		},
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
