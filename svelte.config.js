da2f6c33d5bd645bafb9037d6773051a21746b66
Alan Keown <BurrellCreekCommunity@gmail.com>
2021-07-24 17:08:07 +1000

    Another try

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,16 @@
+
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
 	kit: {
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
+		adapter: adapter(),
 		// Comment the paths if wants to run in dev mode.
 		paths: {
-			base: '/Strengthening-Our-Community/mulligans-lane',
-			assets: '/Strengthening-Our-Community/mulligans-lane'
+			base: '/mulligans-lane',
+			assets: '/mulligans-lane'
 		},
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
